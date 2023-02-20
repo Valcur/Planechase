55ce28882f2e92b00fea5d96361c50c1faa144f8
Loic D <loic.danj@gmail.com>
2023-02-20 11:54:28 +0100

    Initial Commit

---
+++ Planechase/PlanechaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanechaseApp.swift
+//  Planechase
+//
+//  Created by Loic D on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlanechaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
