fa57be1de140df38b2adf93a24b16d348c77df04
Loic D <loic.danj@gmail.com>
2024-02-20 13:17:09 +0100

    Scryfall download fix

--- Planechase/ContentManager/ContentManagerViewModel+Scryfall.swift
+++ Planechase/ContentManager/ContentManagerViewModel+Scryfall.swift
@@ -54,12 +54,14 @@ extension ContentManagerViewModel {
         var cards: [Card] = []
         
         for plane in planechaseURLs.data {
-            cards.append(Card(id: plane.oracleID,
-                              imageURL: plane.imageUris.large,
-                              state: .showed,
-                              cardSets: [CardSet.cardSetForCode(setCode)],
-                              cardType: CardTypeLine.typeForTypeLine(plane.typeLine)
-                             ))
+            if let planeId = plane.oracleID, let planeImage = plane.imageUris.large {
+                cards.append(Card(id: planeId,
+                                  imageURL: planeImage,
+                                  state: .showed,
+                                  cardSets: [CardSet.cardSetForCode(setCode)],
+                                  cardType: CardTypeLine.typeForTypeLine(plane.typeLine)
+                                 ))
+            }
         }
 
         self.addToCollection(cards)
@@ -71,9 +73,9 @@ extension ContentManagerViewModel {
     
     // MARK: - Welcome
     struct PlanechaseURLs: Codable {
-        let object: String
-        let totalCards: Int
-        let hasMore: Bool
+        let object: String?
+        let totalCards: Int?
+        let hasMore: Bool?
         let data: [Datum]
 
         enum CodingKeys: String, CodingKey {
@@ -86,137 +88,27 @@ extension ContentManagerViewModel {
 
     // MARK: - Datum
     struct Datum: Codable {
-        let object: Object
-        let id, oracleID: String
-        let multiverseIDS: [Int]
-        let mtgoID: Int?
-        let tcgplayerID, cardmarketID: Int?
-        let name: String
-        let lang: Lang
-        let releasedAt: String
-        let uri, scryfallURI: String
-        let layout: String
-        let highresImage: Bool
-        let imageStatus: ImageStatus
+        //let object: Object
+        let id, oracleID: String?
+        let name: String?
+        let lang: String?
         let imageUris: ImageUris
-        let manaCost: String
-        let cmc: Int
-        let typeLine, oracleText: String?
-        let colors: [JSONAny]
-        let colorIdentity, keywords: [String]
-        let legalities: Legalities
-        let games: [Game]
-        let reserved, foil, nonfoil: Bool
-        let finishes: [Finish]
-        let oversized, promo, reprint, variation: Bool
-        let setID: String
-        let datumSet: String
-        let setName: String
-        let setType: SetType
-        let setURI, setSearchURI, scryfallSetURI, rulingsURI: String
-        let printsSearchURI: String
-        let collectorNumber: String
-        let digital: Bool
-        let rarity: Rarity
-        let cardBackID, artist: String
-        let artistIDS: [String]
-        let illustrationID: String
-        let borderColor: BorderColor
-        let frame: String
-        let fullArt, textless, booster, storySpotlight: Bool
-        let prices: [String: String?]
-        let relatedUris: RelatedUris
-        let purchaseUris: PurchaseUris
-        let allParts: [AllPart]?
-        let producedMana: [String]?
+        let typeLine: String?
 
         enum CodingKeys: String, CodingKey {
-            case object, id
+            case id
             case oracleID = "oracle_id"
-            case multiverseIDS = "multiverse_ids"
-            case mtgoID = "mtgo_id"
-            case tcgplayerID = "tcgplayer_id"
-            case cardmarketID = "cardmarket_id"
             case name, lang
-            case releasedAt = "released_at"
-            case uri
-            case scryfallURI = "scryfall_uri"
-            case layout
-            case highresImage = "highres_image"
-            case imageStatus = "image_status"
             case imageUris = "image_uris"
-            case manaCost = "mana_cost"
-            case cmc
             case typeLine = "type_line"
-            case oracleText = "oracle_text"
-            case colors
-            case colorIdentity = "color_identity"
-            case keywords, legalities, games, reserved, foil, nonfoil, finishes, oversized, promo, reprint, variation
-            case setID = "set_id"
-            case datumSet = "set"
-            case setName = "set_name"
-            case setType = "set_type"
-            case setURI = "set_uri"
-            case setSearchURI = "set_search_uri"
-            case scryfallSetURI = "scryfall_set_uri"
-            case rulingsURI = "rulings_uri"
-            case printsSearchURI = "prints_search_uri"
-            case collectorNumber = "collector_number"
-            case digital, rarity
-            case cardBackID = "card_back_id"
-            case artist
-            case artistIDS = "artist_ids"
-            case illustrationID = "illustration_id"
-            case borderColor = "border_color"
-            case frame
-            case fullArt = "full_art"
-            case textless, booster
-            case storySpotlight = "story_spotlight"
-            case prices
-            case relatedUris = "related_uris"
-            case purchaseUris = "purchase_uris"
-            case allParts = "all_parts"
-            case producedMana = "produced_mana"
         }
-    }
-
-    // MARK: - AllPart
-    struct AllPart: Codable {
-        let object, id, component, name: String
-        let typeLine: String
-        let uri: String
-
-        enum CodingKeys: String, CodingKey {
-            case object, id, component, name
-            case typeLine = "type_line"
-            case uri
-        }
-    }
-
-    enum BorderColor: String, Codable {
-        case black = "black"
-    }
-
-    enum Finish: String, Codable {
-        case nonfoil = "nonfoil"
-        case foil = "foil"
-    }
-
-    enum Game: String, Codable {
-        case mtgo = "mtgo"
-        case paper = "paper"
-    }
-
-    enum ImageStatus: String, Codable {
-        case highresScan = "highres_scan"
-        case lowres = "lowres"
     }
 
     // MARK: - ImageUris
     struct ImageUris: Codable {
-        let small, normal, large: String
-        let png: String
-        let artCrop, borderCrop: String
+        let small, normal, large: String?
+        let png: String?
+        let artCrop, borderCrop: String?
 
         enum CodingKeys: String, CodingKey {
             case small, normal, large, png
@@ -225,55 +117,8 @@ extension ContentManagerViewModel {
         }
     }
 
-    enum Lang: String, Codable {
-        case en = "en"
-    }
-
-    // MARK: - Legalities
-    struct Legalities: Codable {
-        let standard, future, historic, gladiator: String
-        let pioneer, explorer, modern, legacy: String
-        let pauper, vintage, penny, commander: String
-        let brawl, historicbrawl, alchemy, paupercommander: String
-        let duel, oldschool, premodern: String
-    }
-
-    enum Object: String, Codable {
-        case card = "card"
-    }
-
-    // MARK: - PurchaseUris
-    struct PurchaseUris: Codable {
-        let tcgplayer, cardmarket, cardhoarder: String
-    }
-
-    enum Rarity: String, Codable {
-        case common = "common"
-        case uncommon = "uncommon"
-        case rare = "rare"
-        case mythic = "mythic"
-    }
-
-    // MARK: - RelatedUris
-    struct RelatedUris: Codable {
-        let gatherer: String?
-        let tcgplayerInfiniteArticles, tcgplayerInfiniteDecks, edhrec: String?
-
-        enum CodingKeys: String, CodingKey {
-            case gatherer
-            case tcgplayerInfiniteArticles = "tcgplayer_infinite_articles"
-            case tcgplayerInfiniteDecks = "tcgplayer_infinite_decks"
-            case edhrec
-        }
-    }
-
-    enum SetType: String, Codable {
-        case planechase = "planechase"
-        case commander = "commander"
-    }
-
     // MARK: - Encode/decode helpers
-
+/*
     class JSONNull: Codable, Hashable {
 
         public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
@@ -513,5 +358,5 @@ extension ContentManagerViewModel {
             }
         }
     }
-
+*/
 }
